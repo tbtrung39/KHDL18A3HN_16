a860b106bb8416ba6a268e9df0131c5b5fb23237
Bich15052006 <dtbich.24174600151@sv.uneti.edu.vn>
2025-02-22 23:05:26 +0700

    bai5.py

---
+++ lab_1/bai5.py
@@ -0,0 +1,4 @@
+a, b, c = map(float,input('Nhập toạ độ vecto A: ').split())
+a1, b1, c1 = map(float,input('Nhap toạ độ vecto B: ').split())
+tich_vo_huong = a*a1 + b*b1 + c*c1
+print(f'Tích vô hướng của 2 vecto là: {tich_vo_huong:.2f}')
