65b77735be026e7cbc9f8caf38a5f0a1a42545c3
Nguyen_Nhu_Huy_Hoang <nnhhoang.24174600137@sv.uneti.edu.vn>
2025-03-26 22:47:39 +0700

    Hoàn thành lab05

--- Lab_05/cau1.py
+++ Lab_05/cau1.py
@@ -1,30 +1,12 @@
-while True:
-    n = int(input("Nhập số nguyên dương n: "))
-    if n > 0:
-        break
-    print("Vui lòng nhập một số nguyên dương!")
+#cach 1
+input_str = input("Nhap chuoi ky tu: ")
+digit_count = 0
+for char in input_str:
+    if char.isdigit():
+        digit_count += 1
+print(f"so ky tu là so trong chuoi: {digit_count}")
+# cach 2
+input_str = input("Nhap chuoi ky tu: ")
+digit_count = sum(1 for char in input_str if char.isdigit())
+print(f"So ky tu la so trong chuoi: {digit_count}")
 
-# a) Tính S4 = 1^2 + 2^2 + 3^2 + ... + n^2
-S4 = 0
-i = 1
-while i <= n:
-    S4 += i**2
-    i += 1
-
-# b) Tính S5 = 1^3 + 3^3 + 5^3 + ... + (2n+1)^3
-S5 = 0
-i = 1
-while i <= (2*n+1):
-    S5 += i**3
-    i += 2  
-
-# c) Tính S6 = 2^4 + 4^4 + 6^4 + ... + (2n)^4
-S6 = 0
-i = 2
-while i <= (2*n):
-    S6 += i**4
-    i += 2  
-
-print("S4 =", S4)
-print("S5 =", S5)
-print("S6 =", S6)
