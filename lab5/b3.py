cb28c66e5826152040eda4890017ff4175f9bcbc
Nguyen-Van-Diey <nvduy.24174600125@sv.uneti.edu.vn>
2025-04-02 21:13:10 +0700

    first commit

--- lab5/b3.py
+++
@@ -1,9 +0,0 @@
-so_thap_phan = int(input("Nhập số nguyên dương: "))
-chuoi_nhi_phan = ""
-if so_thap_phan == 0:
-    chuoi_nhi_phan = "0"
-else:
-    while so_thap_phan > 0:
-        chuoi_nhi_phan = str(so_thap_phan % 2) + chuoi_nhi_phan
-        so_thap_phan = so_thap_phan // 2
-print("Chuỗi nhị phân là:", chuoi_nhi_phan)
