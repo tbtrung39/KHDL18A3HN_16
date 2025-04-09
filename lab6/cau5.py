a4bb21a29fa3981b8c3cd9ec822cd53f5345fc78
Nguyen-Van-Diey <nvduy.24174600125@sv.uneti.edu.vn>
2025-04-09 20:22:57 +0700

    first commit

--- lab6/cau5.py
+++
@@ -1,5 +0,0 @@
-import random
-
-# Câu 5: Tạo list A gồm 1000 số ngẫu nhiên [1, 99999]
-A = [random.randint(1, 99999) for _ in range(1000)]
-print ("chương trình đã tạo list A gồm 1000 số ngẫu nhiêu là:",A)
--- lab6/cau8.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Nhập số phần tử Fibonacci (n): "))
-
-# Tạo dãy Fibonacci bằng list comprehension
-fibonacci = [0, 1]  # Khởi tạo 2 phần tử đầu
-[fibonacci.append(fibonacci[-1] + fibonacci[-2]) for _ in range(n - 2)]
-
-# In kết quả cách nhau bằng dấu ","
-print("Dãy Fibonacci:", ", ".join(map(str, fibonacci[:n])))
