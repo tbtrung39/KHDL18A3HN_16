09cdf1724bfeb9520861ed86e948d106469f1116
Nguyen-Van-Diey <nvduy.24174600125@sv.uneti.edu.vn>
2025-05-23 23:20:57 +0700

    hoan thanh bai tap lab 12

--- lab 11/cau 2/cau 2.py
+++
@@ -1,8 +0,0 @@
-def sap_xep_tang_dan(infile, outfile):
-    with open(infile, 'r') as f:
-        numbers = list(map(int, f.read().strip().split()))
-
-    numbers.sort()
-
-    with open(outfile, 'w') as f:
-        f.write(' '.join(map(str, numbers)))
